9dbf88fd975f515088d8dc0e8f0f13b888f2ca59
LevikV <alex@kplus79.ru>
2020-09-14 18:51:36 +1000

    Исправил отображение works

--- public_html/views/works/index.php
+++ public_html/views/works/index.php
@@ -1,8 +1,6 @@
 <?php if ($works) : ?>
-    <?php foreach ($works as $work): ?>
-        <h2><?=$work['joblist_id'];?></h2>
-        <p><?=$work['price'];?></p>
-    <?php endforeach; ?>
-<?php else: ?>
+        <h2><?=$works['joblist_id'];?></h2>
+        <p><?=$works['count'];?></p>
+    <?php else: ?>
     <h2>Таблица работ пуста!</h2>
 <?php endif; ?>
