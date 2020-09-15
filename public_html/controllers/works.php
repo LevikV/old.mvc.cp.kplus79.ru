9e5fe5094baa1fc05baa60b2e7b6cbb4e72d560c
Алексей Димов <alex@kplus79.ru>
2020-09-15 23:11:53 +1000

    15092020 2311

--- public_html/controllers/works.php
+++ public_html/controllers/works.php
@@ -4,14 +4,16 @@ class Controller_Works extends Controller_Base {
     public $datelastweek;
 
     function index(){
-        //$date1 = date("Y-m-d", $this->getLastWeek());
-        //$date2 = date("Y-m-d", time());
-        //where' => '`date` >= AND `date <= `
+        $date1 = $this->getLastWeek();
+        $date2 = date("Y-m-d", time());
+        //'where' => 'id=1',
+        //'where' => "date >= $date1 AND date <= $date2"
+        //'where' => "date >= 2020-09-01 AND date <= 2020-09-30"
         $select = array(
-            'where' => 'id=1',
-        );
+            'where' => "date >= $date1"
+            );
         $model = new Model_Works($select);
-        $works = $model->getOneRow();
+        $works = $model->getAllRows();
         //
         //
         $this->template->vars('datelastweek', $this->getLastWeek());
@@ -26,31 +28,38 @@ class Controller_Works extends Controller_Base {
         switch ($d['wday']){
             case 0:
                 $timelastweek = $timenow - 13 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 1:
                 $timelastweek = $timenow - 7 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 2:
                 $timelastweek = $timenow - 8 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 3:
                 $timelastweek = $timenow - 9 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 4:
                 $timelastweek = $timenow - 10 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 5:
                 $timelastweek = $timenow - 11 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
             case 6:
                 $timelastweek = $timenow - 12 * 86400;
-                $datelastweek = getdate($timelastweek);
+                $datelastweek = date("Y-m-d",$timelastweek);
+                //$datelastweek = getdate($timelastweek);
                 break;
         }
         return $datelastweek;
--- public_html/views/works/index.php
+++ public_html/views/works/index.php
@@ -1,11 +1,11 @@
 <?php if ($works) : ?>
-        <h2><?=$works['joblist_id'];?></h2>
-        <p><?=$works['count'];?></p>
+        <h2><?//=$works['joblist_id'];?></h2>
+        <p><?//=$works['count'];?></p>
     <?php else: ?>
     <h2>Таблица работ пуста!</h2>
 <?php endif; ?>
 <?php
-    echo $datelastweek['mday'];
+    echo $datelastweek;
     //foreach ($d as $key => $val) {
       //  echo $key;
       //  echo "</br>";
