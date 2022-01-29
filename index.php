f748957d2ef5ea7ed6edc115835e08e5c1bbe369
LevikV <alex@kplus79.ru>
2022-01-29 20:34:13 +1000

    Переместил в верхний каталог. Запустил!

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 error_reporting(E_ALL);
 
 //Подключим конфиг
-include ('config.php');
+include('config.php');
 
 //Соединяемся с БД
 $dbObject = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 
 //Подключаем ядро сайта
-include (SITE_PATH . DS . 'core' . DS . 'core.php');
+include(SITE_PATH . DS . 'core' . DS . 'core.php');
 
 //Загружаем роутер
 $router = new Router($registry);
--- views/layouts/first_layouts.php
+++ views/layouts/first_layouts.php
@@ -36,7 +36,7 @@
                 <a class="nav-link" href="/">Главная</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/works">Работы</a>
+                <a class="nav-link" href="../../index.php">Работы</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Отчеты</a>
